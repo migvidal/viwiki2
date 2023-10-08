836a329ce7ec1f259e98d323b90a42e774143f12
migvidal <mvidal@air-institute.com>
2023-10-08 13:58:42 +0200

    Add network related dependencies. Add internet permission to manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,13 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
     // /Room
 
+    // Network
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.10")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    // /Network
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
