99c6f3c3b77fb693943ef2d6b864dc1fbcd4cb31
migvidal <mvidal@air-institute.com>
2023-10-08 14:12:33 +0200

    Add code for WikiMedia API service

---
+++ app/src/main/java/com/migvidal/viwiki2/data/network/ApiCommons.kt
@@ -0,0 +1,18 @@
+package com.migvidal.viwiki2.data.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.logging.HttpLoggingInterceptor
+
+object ApiCommons {
+    /**
+     * Converter from Kotlin to JSON
+     */
+    val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    /**
+     * Logs the request URL to console
+     */
+    val loggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BASIC)
+}
---
+++ app/src/main/java/com/migvidal/viwiki2/data/network/WikimediaApi.kt
@@ -0,0 +1,62 @@
+package com.migvidal.viwiki2.data.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * WikiMedia API. Used to get the "today" data.
+ */
+
+/**
+ * Base URL for the Wikimedia API
+ */
+private const val WikimediaBaseUrl: String = "https://api.wikimedia.org/"
+
+/**
+ * HTTP client
+ */
+private val okHttpClient = OkHttpClient.Builder()
+    .build()
+
+/**
+ * Retrofit client, for creating the API service
+ */
+private val retrofit = Retrofit.Builder()
+    .baseUrl(WikimediaBaseUrl)
+    .client(okHttpClient)
+    .addConverterFactory(MoshiConverterFactory.create(ApiCommons.moshi))
+    .build()
+
+/**
+ * Interface for Retrofit to handle queries
+ */
+interface WikiMediaApiService {
+    @GET(
+        "/feed/v1/wikipedia/en/featured/" +
+                "{yyyy}/{mm}/{dd}"
+    )
+    /**
+     * Fetches the "today" response for a certain date
+     * @param yyyy The year
+     * @param mm The month
+     * @param dd The day
+     */
+    suspend fun getFeatured(
+        @Path("yyyy") yyyy: String,
+        @Path("mm") mm: String,
+        @Path("dd") dd: String
+    ): //TodoModelClass
+}
+
+
+object WikiMediaApiImpl {
+    /**
+     * Service for the WikiMedia API
+     */
+    val wikiMediaApiService: WikiMediaApiService by lazy {
+        retrofit.create(WikiMediaApiService::class.java)
+    }
+}
