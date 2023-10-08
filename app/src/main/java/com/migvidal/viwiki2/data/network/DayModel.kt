7425bf228de251b36e04a0d69b80a9b0a15e2b1f
migvidal <mvidal@air-institute.com>
2023-10-08 14:30:38 +0200

    Model the wikimedia api for the "today" data

---
+++ app/src/main/java/com/migvidal/viwiki2/data/network/DayModel.kt
@@ -0,0 +1,58 @@
+package com.migvidal.viwiki2.data.network
+
+import com.migvidal.viwiki2.data.database.entities.Article
+import com.squareup.moshi.Json
+
+internal data class Image(
+    val source: String,
+    val width: Int,
+    val height: Int,
+)
+
+internal data class DayResponse(
+    @Json(name = "tfa")
+    val featuredArticle: FeaturedArticle?,
+
+    @Json(name = "mostread")
+    val mostRead: MostRead?,
+
+    val image: DayImage?,
+
+    @Json(name = "onthisday")
+    val onThisDay: List<OnThisDay>,
+)
+
+internal data class FeaturedArticle(
+    val type: String,
+    val title: String,
+    @Json(name = "displaytitle")
+    val displayTitle: String,
+    @Json(name = "originalimage")
+    val originalImage: Image,
+    val thumbnail: Image,
+    val description: String,
+    val extract: String,
+    @Json(name = "normalizedtitle")
+    val normalizedTitle: String,
+)
+
+internal data class MostRead(
+    val date: String,
+    val articles: List<Article>,
+)
+
+internal class OnThisDay
+
+internal data class DayImage(
+    val title: String,
+    val thumbnail: Image,
+    val image: Image,
+    @Json(name = "filepage")
+    val filePage: String,
+    val description: Description,
+) {
+    data class Description(
+        val text: String,
+        val lang: String,
+    )
+}
