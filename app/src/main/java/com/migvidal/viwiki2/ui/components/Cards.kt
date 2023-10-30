52b0646a134ce4202ac269d0cb5ece671e75fc72
migvidal <mvidal@air-institute.com>
2023-10-30 13:10:37 +0100

    Add previews to components

--- app/src/main/java/com/migvidal/viwiki2/ui/components/Cards.kt
+++ app/src/main/java/com/migvidal/viwiki2/ui/components/Cards.kt
@@ -1,15 +1,21 @@
 package com.migvidal.viwiki2.ui.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.migvidal.viwiki2.ui.theme.ViWiki2Theme
 
 private val cardBorder: BorderStroke
     @Composable get() = BorderStroke(
@@ -50,5 +56,20 @@ fun CustomCard(
         elevation = cardElevation,
     ) {
         content()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+fun CustomCardPreview() {
+    ViWiki2Theme {
+        Surface {
+            CustomCard(modifier = Modifier.padding(16.dp), onClick = {}) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = "Lorem ipsum dolor sit amet consectetur"
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/migvidal/viwiki2/ui/components/Headings.kt
+++ app/src/main/java/com/migvidal/viwiki2/ui/components/Headings.kt
@@ -1,13 +1,18 @@
 package com.migvidal.viwiki2.ui.components
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.migvidal.viwiki2.ui.theme.ViWiki2Theme
 
 @Composable
 fun SectionHeading(modifier: Modifier = Modifier, text: String) {
@@ -17,4 +22,17 @@ fun SectionHeading(modifier: Modifier = Modifier, text: String) {
         style = MaterialTheme.typography.titleSmall.copy(letterSpacing = 1.2.sp),
         fontWeight = FontWeight.Light
     )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+fun SectionHeadingPreview() {
+    ViWiki2Theme {
+        Surface {
+            Column(modifier = Modifier.padding(8.dp)) {
+                SectionHeading(text = "This is the heading")
+                Text(text = "This is more text")
+            }
+        }
+    }
 }
