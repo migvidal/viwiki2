2cbf03c870455a03f47e4e56ca9a59deb34d1da3
migvidal <mvidal@air-institute.com>
2023-12-02 12:25:27 +0100

    Model the "search" response

---
+++ app/src/main/java/com/migvidal/viwiki2/data/network/search/SearchResponseModel.kt
@@ -0,0 +1,40 @@
+package com.migvidal.viwiki2.data.network.search
+
+import com.squareup.moshi.Json
+
+data class SearchResponseModel(
+    @Json(name = "batchcomplete")
+    val batchComplete: String,
+    val `continue`: Continue,
+    val query: Query
+)
+
+data class Continue(
+    val `continue`: String,
+    @Json(name = "sroffset")
+    val srOffset: Int
+)
+
+data class Query(
+    val search: List<Search>,
+    @Json(name = "searchinfo")
+    val searchInfo: SearchInfo
+) {
+    data class SearchInfo(
+        @Json(name = "totalhits")
+        val totalHits: Int
+    )
+
+    data class Search(
+        val ns: Int,
+        @Json(name = "pageid")
+        val pageId: Int,
+        val size: Int,
+        val snippet: String,
+        val timestamp: String,
+        val title: String,
+        @Json(name = "wordcount")
+        val wordCount: Int
+    )
+}
+
--- app/src/main/java/com/migvidal/viwiki2/data/network/search/SearchResultsModel.kt
+++
@@ -1,3 +0,0 @@
-package com.migvidal.viwiki2.data.network.search
-
-// TODO MODEL SEARCH RESULTS
