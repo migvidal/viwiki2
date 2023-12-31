18ca789bc004014052db9e49c08ee6f46a27a2d9
migvidal <mvidal@air-institute.com>
2023-12-31 14:26:23 +0100

    Article screen: make image fill screen, adjust padding

--- app/src/main/java/com/migvidal/viwiki2/ui/screens/article_screen/ArticleScreen.kt
+++ app/src/main/java/com/migvidal/viwiki2/ui/screens/article_screen/ArticleScreen.kt
@@ -4,6 +4,9 @@ import android.webkit.WebView
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
@@ -13,6 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.migvidal.viwiki2.data.network.article.ArticleResponseModel
 import com.migvidal.viwiki2.data.repository.Repository
@@ -44,22 +49,32 @@ fun AnimatedVisibilityScope.ArticleScreen(
                 Text(text = article.title, style = MaterialTheme.typography.displayMedium)
                 article.original?.let {
                     val aspectRatio = it.width / it.height.toFloat()
-                    CustomAsyncImage(model = it.source, aspectRatio = aspectRatio)
+                    val screenWidth = LocalConfiguration.current.screenWidthDp
+                    CustomAsyncImage(
+                        modifier = Modifier
+                            .wrapContentWidth(unbounded = true)
+                            .width(screenWidth.dp),
+                        model = it.source,
+                        aspectRatio = aspectRatio
+                    )
                 }
                 val cssTextColor = if (isSystemInDarkTheme()) "white" else "black"
-                AndroidView(factory = { context ->
-                    val webView = WebView(context)
-                    webView.setBackgroundColor(Color.Transparent.toArgb())
-                    webView
-                }, update = { view ->
-                    val htmlString = "<html><head>" +
-                            "<style type=\"text/css\">body { color: $cssTextColor; }</style>" +
-                            "</head>" +
-                            "<body>" +
-                            article.extract +
-                            "</body></html>"
-                    view.loadData(htmlString, "text/html; charset=utf-8", "UTF-8")
-                })
+                AndroidView(
+                    modifier=Modifier.padding(vertical = 24.dp),
+                    factory = { context ->
+                        val webView = WebView(context)
+                        webView.setBackgroundColor(Color.Transparent.toArgb())
+                        webView
+                    },
+                    update = { view ->
+                        val htmlString = "<html><head>" +
+                                "<style type=\"text/css\">body { color: $cssTextColor; }</style>" +
+                                "</head>" +
+                                "<body>" +
+                                article.extract +
+                                "</body></html>"
+                        view.loadData(htmlString, "text/html; charset=utf-8", "UTF-8")
+                    })
             }
         }
     }
